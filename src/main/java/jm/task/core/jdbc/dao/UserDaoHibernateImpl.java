80128c913cec269b3985ac179876f56f624a5a05
yungbrave <SergeyIlinR@gmail.com>
2023-06-07 19:09:36 +0300

    задача 1.1.5 (сырой вариант)

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,14 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -12,31 +19,114 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
 
+        try (sessionFactory; session) {
+            session.beginTransaction();
+
+            session.createSQLQuery("create table if not exists users(" +
+                    "id INT AUTO_INCREMENT PRIMARY KEY," +
+                    "name VARCHAR(45) NOT NULL," +
+                    "lastname VARCHAR(45) NOT NULL," +
+                    "age TINYINT(10) NOT NULL)");
+
+            session.getTransaction().commit();
+
+        } catch (RuntimeException e) {
+            sessionFactory.getCurrentSession().getTransaction().rollback();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        Configuration configuration = new Configuration();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
 
+        try (sessionFactory; session) {
+            session.beginTransaction();
+
+            session.createSQLQuery("DROP TABLE IF EXISTS users");
+
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            sessionFactory.getCurrentSession().getTransaction().rollback();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try (sessionFactory; session) {
+            session.beginTransaction();
+
+            User user = new User(name, lastName, age);
+            session.save(user);
+
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            sessionFactory.getCurrentSession().getTransaction().rollback();
+        }
 
     }
 
     @Override
     public void removeUserById(long id) {
+        Configuration configuration = new Configuration();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
 
+
+        try (sessionFactory; session) {
+            session.beginTransaction();
+
+            User user = session.get(User.class, id);
+            session.delete(user);
+
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            sessionFactory.getCurrentSession().getTransaction().rollback();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        Configuration configuration = new Configuration();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try (sessionFactory; session) {
+            session.beginTransaction();
+
+            users = session.createQuery("FROM User").getResultList();
+
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            sessionFactory.getCurrentSession().getTransaction().rollback();
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
+        Configuration configuration = new Configuration();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
 
+        try (sessionFactory; session) {
+            session.beginTransaction();
+
+            session.createQuery("delete from User").executeUpdate();
+
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            sessionFactory.getCurrentSession().getTransaction().rollback();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,21 +1,22 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Table
+@Table (name = "users")
+@Entity
 public class User {
     @Id
+    @Column (name = "id")
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column (name = "name")
     private String name;
 
-    @Column
+    @Column (name = "lastname")
     private String lastName;
 
-    @Column
+    @Column (name = "age")
     private Byte age;
 
     public User() {
@@ -27,6 +28,7 @@ public class User {
         this.lastName = lastName;
         this.age = age;
     }
+
 
     public Long getId() {
         return id;
