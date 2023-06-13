420c2e8117ad69b1d05db4fa83b5e6328c544c6f
yungbrave <SergeyIlinR@gmail.com>
2023-06-13 18:27:03 +0300

    1.1.4 задача, добавлен вывод через foreach

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,10 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +15,10 @@ public class Main {
         userService.saveUser("Nikola", "Tesla", (byte) 22);
         userService.saveUser("Albert", "Einstein", (byte) 25);
         userService.saveUser("Aleksandr", "Popov", (byte) 30);
-        System.out.println(userService.getAllUsers());
+        List<User> users = userService.getAllUsers();
+        for (User user: users) {
+            System.out.println(user);
+        }
         userService.cleanUsersTable();
         userService.dropUsersTable();
 
